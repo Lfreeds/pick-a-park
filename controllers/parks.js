03f3cdcacca0bd76cd5e7fd26a047702a32240e3
Lucas <lucas.friedmann35@live.ca>
2022-10-26 11:57:32 -0400

    add park index functionality

--- controllers/parks.js
+++ controllers/parks.js
@@ -1,18 +1,25 @@
 const Park = require("../models/park");
 
+function index(req, res) {
+  Park.find({}, function (err, parks) {
+    res.render("parks/index", { title: "All Parks", parks });
+  });
+}
+
 function newPark(req, res) {
-  res.render("parks/new");
+  res.render("parks/new", { title: "Add Park" });
 }
 
 function create(req, res) {
   const park = new Park(req.body);
   park.save(function (err) {
     if (err) return res.render("parks/new");
-    res.redirect("/parks/new");
+    res.redirect("/parks");
   });
 }
 
 module.exports = {
   new: newPark,
   create,
+  index,
 };
--- routes/parks.js
+++ routes/parks.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const parksCtrl = require("../controllers/parks");
 
+router.get("/", parksCtrl.index);
 router.get("/new", parksCtrl.new);
 router.post("/", parksCtrl.create);
 
